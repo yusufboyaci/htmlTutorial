64f1c1290670d072f92a7bcc88d535c9a49dadc5
yusufboyaci <boyacyusuf@gmail.com>
2021-10-26 19:19:53 +0300

    ASP.NET CORE MVC Yazılımcıları için jQuery ve jQuery UI eğitimi

---
+++ Alper Tunga Ajax Dersi/ders3.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // $('tr:first').css('color','orange');
+    // $('tr:last').css('background-color','blue');
+    $('tr:odd').css("background-color",'black');
+    $('tr:odd').css("color",'black');
+});
